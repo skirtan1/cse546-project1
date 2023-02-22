404b81d961d436954f358650f0bbf861bfcc2a8d
Parth Shah <parthshah576@gmail.com>
2023-02-22 01:02:53 -0700

    fix merge conflict in wt_server

--- wt_server.py
+++ wt_server.py
@@ -12,10 +12,14 @@ import base64
 from uuid import uuid4
 import concurrent.futures
 import time
+import threading
+
+resultQueue = dict()
+writeLock = threading.Lock()
 
 class WebWorker:
 
-    def __init__(self, config) -> None:
+    def __init__(self, config, queue_url, sqsClient) -> None:
         self.app = Flask(__name__)
         self.app.config['SERVER_TIMEOUT'] = 120
         self.app.config['TIMEOUT'] = 120
@@ -34,8 +38,10 @@ class WebWorker:
         self.requestQueue = sqs.get_queue_by_name(QueueName=config.get('sqs','request_queue'))
         self.responseQueue = sqs.get_queue_by_name(QueueName=config.get('sqs','response_queue'))
 
-        self.queue_url = config.get('sqs','response_queue_url')
-        self.sqsClient = boto3.client('sqs', config=client_config)
+        #self.queue_url = config.get('sqs','response_queue_url')
+        #self.sqsClient = boto3.client('sqs', config=client_config)
+        self.queue_url = queue_url
+        self.sqsClient = sqsClient
 
     def index(self):
         return "Hello World"
@@ -47,15 +53,13 @@ class WebWorker:
         mfile.write(file.read())
         safe_upload(client=self.s3, bucket=self.input_bucket,
                     key=filename, data=mfile, content_type="image/png")
-        #resp = requests.post(url=self.remote_url, json={
-        #    "filename": filename,
-        #    "Key": 1
-        #})
         logging.info("Sending msg to request queue: {}".format(filename))
         messageId = self.write_to_msgq(filename)
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = executor.submit(poll_resp_q, messageId, self.queue_url, self.sqsClient)
-            result = future.result()
+        while True:
+            result = getMessageById(messageId, self.queue_url, self.sqsClient)
+            if result != 'None':
+                break
+            time.sleep(2)
         return result
 
     def write_to_msgq(self, message) -> str:
@@ -66,9 +70,24 @@ class WebWorker:
         except Exception as e:
             print(f'Error: {str(e)}')
 
-def poll_resp_q(messageId:str, queue_url: str, sqsClient) -> str:
+def getMessageById(messageId:str, queue_url, sqsClient) -> str:
+    global resultQueue
+    with writeLock:
+        if messageId in resultQueue:
+            message = resultQueue[messageId]
+            sqsClient.delete_message(
+                QueueUrl=queue_url,
+                ReceiptHandle=message['receipt_handle']
+            )
+            del resultQueue[messageId]
+        else:
+            return 'None'
+    return message['result']
+
+
+def poll_resp_q(queue_url: str, sqsClient) -> str:
     try:
-        logging.info("Starting to poll response queue, msgId: {}".format(messageId))
+        logging.info("Starting to poll response queue")
         while True:
             response = sqsClient.receive_message(
                 QueueUrl=queue_url,
@@ -78,7 +97,7 @@ def poll_resp_q(messageId:str, queue_url: str, sqsClient) -> str:
                 MessageAttributeNames=[
                     'All'
                 ],
-                VisibilityTimeout=0,
+                VisibilityTimeout=180,
                 WaitTimeSeconds=0
             )
 
@@ -87,18 +106,18 @@ def poll_resp_q(messageId:str, queue_url: str, sqsClient) -> str:
                 time.sleep(3)
                 continue
 
+            global resultQueue
+
             for message in response['Messages']:
                 if 'MessageAttributes' in message:
                     messageAttr = message['MessageAttributes']
-                    if messageAttr['messageId']['StringValue'] == messageId:
-                        receipt_handle = message['ReceiptHandle']
-                        result = message['Body']
-                        sqsClient.delete_message(
-                            QueueUrl=queue_url,
-                            ReceiptHandle=receipt_handle
-                        )
-                        return result
-            time.sleep(3)
+                    messageId = messageAttr['messageId']['StringValue']
+                    with writeLock:
+                        resultQueue[messageId] = {
+                            'result': message['Body'],
+                            'receipt_handle' : message['ReceiptHandle']
+                        }
+            time.sleep(10)
 
 
     except Exception as e: 
@@ -109,10 +128,22 @@ if __name__ == "__main__":
     config = ConfigParser()
     config.read('wt_config.ini')
 
+    client_config=botocore.config.Config(
+        max_pool_connections=int(config.get('boto', 'max_pool_connections'))
+    )
+    queue_url = config.get('sqs','response_queue_url')
+    sqsClient = boto3.client('sqs', config=client_config)
+
     logging.basicConfig(format='%(asctime)s %(message)s',
                         datefmt='%m/%d/%Y %I:%M:%S %p',
                         level=int(config.get('logging', 'level')))
-    worker = WebWorker(config=config)
+    worker = WebWorker(config=config, queue_url=queue_url, sqsClient=sqsClient)
+
+    #with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+    #    future = executor.submit(poll_resp_q, queue_url, sqsClient)
+    t = threading.Thread(target=poll_resp_q, args=(queue_url, sqsClient))
+    t.start()
+
     host=config.get('flask', 'host')
     port=int(config.get('flask', 'port'))
     worker.app.run(host, port)
